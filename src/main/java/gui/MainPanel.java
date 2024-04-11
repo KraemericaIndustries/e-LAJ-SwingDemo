655aea08f337086a6d12148c37a1c65d585f4d81
Bob Kraemer <bob.kraemer.git@gmail.com>
2024-04-11 15:59:34 -0400

    Verified code working through lectures 108: "Adding Components"

--- src/main/java/gui/MainPanel.java
+++ src/main/java/gui/MainPanel.java
@@ -1,6 +1,7 @@
 package gui;
 
-import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -16,14 +17,40 @@ public class MainPanel extends JPanel {
 		var nameLabel = new JLabel("Name:");
 		var passLabel = new JLabel("Password:");
 		
-		var nameField = new JTextField();
-		var passField = new JTextField();
+		var nameField = new JTextField(15);
+		var passField = new JTextField(15);
 		
 		var addButton = new JButton("Save");
 		
-		setLayout(new BorderLayout());
+		setLayout(new GridBagLayout());
 		
-		add(formLabel, BorderLayout.NORTH);
-		add(nameLabel, BorderLayout.CENTER);
+		var gc = new GridBagConstraints();
+		
+		gc.gridx = 0;
+		gc.gridy = 0;
+		
+		gc.gridwidth = 2;
+		add(formLabel, gc);
+		
+		gc.gridy++;
+		gc.gridwidth = 1;
+		
+		gc.gridx = 0;
+		add(nameLabel, gc);
+		
+		gc.gridx = 1;
+		add(nameField, gc);
+		
+		gc.gridy++;
+
+		gc.gridx = 0;
+		add(passLabel, gc);
+		
+		gc.gridx = 1;
+		add(passField, gc);
+		
+		gc.gridy++;
+		gc.gridx = 1;
+		add(addButton, gc);
 	}
 }
