1ac5cc16993f272eda743f1a7a1777bd60f503bf
Bob Kraemer <bob.kraemer.git@gmail.com>
2024-04-11 16:11:41 -0400

    Verified code working through lectures 109: "Weights and Anchors"

--- src/main/java/gui/MainPanel.java
+++ src/main/java/gui/MainPanel.java
@@ -29,6 +29,8 @@ public class MainPanel extends JPanel {
 		gc.gridx = 0;
 		gc.gridy = 0;
 		
+		gc.weightx = 1;
+		
 		gc.gridwidth = 2;
 		add(formLabel, gc);
 		
@@ -36,21 +38,26 @@ public class MainPanel extends JPanel {
 		gc.gridwidth = 1;
 		
 		gc.gridx = 0;
+		gc.anchor = GridBagConstraints.LINE_END;
 		add(nameLabel, gc);
 		
 		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.LINE_START;
 		add(nameField, gc);
 		
 		gc.gridy++;
 
 		gc.gridx = 0;
+		gc.anchor = GridBagConstraints.LINE_END;
 		add(passLabel, gc);
 		
 		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.LINE_START;
 		add(passField, gc);
 		
 		gc.gridy++;
 		gc.gridx = 1;
+		
 		add(addButton, gc);
 	}
 }
